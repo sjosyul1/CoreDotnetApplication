ce3f53bd16e943088dd70d4b7871750cc58c73ae
sai srinath josyula <srinath.5893@gmail.com>
2019-01-16 15:08:13 -0700

    commit edgenuity project

---
+++ Edgenuity/Edgenuity/Models/EmployeeUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Edgenuity.Models
+{
+    public class EmployeeUser
+    {
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [EmailAddress]
+        public string EmailID { get; set; }
+        public string UserID { get; set; }
+    }
+}
